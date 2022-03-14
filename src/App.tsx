4e83ac753f3ee80550372370335023ac204c72d2
Tobias Jacob <tobi.jacob@gmx.net>
2022-03-13 19:51:14 -0500

    Bootstrapped basic app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BayesNode from './components/bayes_node';
+
+const App = () => {
+    return <BayesNode positionX={0} positionY={0} />
+}
+
+export default App;
---
+++ src/components/bayes_node.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './bayes_node.css'
+
+class NodeProps {
+    positionX: number
+    positionY: number
+}
+
+const BayesNode = (props: NodeProps) => {
+    return <div className='BayesNode'>
+        Bayes node
+    </div>
+}
+
+export default BayesNode;
