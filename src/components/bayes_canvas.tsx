b444632358b99c15174baa4828ed2e4b184cdf71
Tobias Jacob <tobi.jacob@gmx.net>
2022-03-13 20:25:18 -0500

    Bayes node is draggable

--- src/components/bayes_canvas.tsx
+++ src/components/bayes_canvas.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import BayesNode from './bayes_node';
+import React, { useState } from 'react';
+import BayesNode, { NodeProps } from './bayes_node';
 
 import './bayes_canvas.css';
 
+class NodeStruct {
+    positionX: number
+    positionY: number
+}
+
 const BayesCanvas = () => {
+    const [nodes, setNodes] = useState<Record<string, NodeStruct>>({
+        "firstNode": {
+            positionX: 400,
+            positionY: 400,
+        }
+    });
+
     return <div className='BayesCanvas'>
         Bayes canvas
-        <BayesNode positionX={400} positionY={800} />
+        {
+            Object.entries(nodes).map(([key, bNode]) => {
+                return <BayesNode key={key} positionX={bNode.positionX} positionY={bNode.positionY} setPosition={
+                    (newX, newY) => {
+                        setNodes({
+                            ...nodes,
+                            [key]: {
+                                positionX: newX,
+                                positionY: newY,
+                            }
+                        })
+                    }
+                }/>
+            })
+        }
     </div>
 }
 
--- src/components/bayes_node.tsx
+++ src/components/bayes_node.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './bayes_node.css';
 
-class NodeProps {
+export class NodeProps {
     positionX: number
     positionY: number
+    setPosition: (newX: number, newY: number) => void
 }
 
 const BayesNode = (props: NodeProps) => {
+    const [dragged, setDragged] = useState(false);
+
+    const mouseDown = (ev: React.MouseEvent<HTMLDivElement>) => {
+        setDragged(true);
+    }
+    const mouseMove = (ev: React.MouseEvent<HTMLDivElement>) => {
+        if (dragged) {
+            props.setPosition(
+                props.positionX + ev.movementX,
+                props.positionY + ev.movementY,
+            )
+        }
+    }
+    const mouseUp = (ev: React.MouseEvent<HTMLDivElement>) => {
+        setDragged(false);
+    }
+
     return <div className='BayesNode' style={{
         top: props.positionY,
         left: props.positionX
-    }}>
+    }} onMouseDown={mouseDown} onMouseMove={mouseMove} onMouseUp={mouseUp}>
         Bayes node
     </div>
 }
